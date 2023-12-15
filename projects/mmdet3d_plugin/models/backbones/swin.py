98ed74e8dc1c4d1930426cce0abcb0227a3d7bb2
barryding <barrydoooit@gmail.com>
2023-12-15 13:26:38 +0100

    test script change and deregister of swintransformer

--- projects/mmdet3d_plugin/models/backbones/swin.py
+++ projects/mmdet3d_plugin/models/backbones/swin.py
@@ -457,7 +457,7 @@ class SwinBlockSequence(BaseModule):
             return x, hw_shape, x, hw_shape
 
 
-@BACKBONES.register_module()
+# @BACKBONES.register_module()
 class SwinTransformer(BaseModule):
     """ Swin Transformer
     A PyTorch implement of : `Swin Transformer:
